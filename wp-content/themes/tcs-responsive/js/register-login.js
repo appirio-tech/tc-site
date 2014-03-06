210148eea7d5a6c20c5e54dde77b849275639c05
Tom Ladendorf <tom@tomladendorf.com>
2014-03-06 10:10:41 -0600

    Modified password strength logic

--- wp-content/themes/tcs-responsive/js/register-login.js
+++ wp-content/themes/tcs-responsive/js/register-login.js
@@ -60,38 +60,28 @@ $(function () {
 
 
   function pwdStrength(pwd) {
-    var n = 0;
-    var c = 0;
-    for (i = 0; i < pwd.length; i++) {
-      if (pwd[i] >= '0' && pwd[i] <= '9') {
-        n++;
-      } else {
-        c++;
-      }
-    }
 
-    if (n > 0 && c > 0 && pwd.length >= 8)
-      return 100.0;
+    var result = 0;
+    if (pwd.trim()=='') return 0;
+    if (pwd[pwd.length-1].match(/\d/)) return -1;
+    if (pwd.length < 7) return -2;
+    if (pwd.length > 30) return -3;
 
-    if (n > 8 || c > 8) return 80.0;
-    if (n > 7 || c > 7) return 80.0;
-    if (n > 6 || c > 6) return 70.0;
-    if (n > 5 || c > 5) return 60.0;
-    if (n > 4 || c > 4) return 50.0;
-    if (n > 3 || c > 3) return 40.0;
-    if (n > 2 || c > 2) return 30.0;
-    if (n > 1 || c > 1) return 20.0;
-    if (n > 0 || c > 0) return 10.0;
-    if (n == 0 && c == 0) return 0;
+    if (pwd.match(/[a-z]/)) result++;
+    if (pwd.match(/[A-Z]/)) result++;
+    if (pwd.match(/\d/)) result++;
+    if (pwd.match(/[\]\[\!\"\#\$\%\&\'\(\)\*\+\,\.\/\:\;\<\=\>\?\@\\\^\_\`\{\|\}\~\-]/)) result++;
+
+    return result;
+
   }
 
   $('input.pwd:password').on('keyup', function () {
     var strength = pwdStrength($(this).val());
-    var strengthId = strength * 6 / 100;
 
     $(".strength .field").removeClass("red").removeClass("green");
     var classname = "red";
-    if (strengthId > 3) {
+    if (strength >= 3) {
       classname = "green";
       $(this).parents(".row").find("span.valid").css("display", "inline-block");
       $(this).closest('.row').find('input:text').removeClass('invalid');
@@ -102,7 +92,7 @@ $(function () {
     }
 
     $(".strength .field").each(function (i, e) {
-      if (i < strengthId) {
+      if (i < strength) {
         $(e).addClass(classname);
       }
     });
