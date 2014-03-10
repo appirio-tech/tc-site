cf8f102f3b1306a737eb71530a9fcea3a7183436
xkurnx-pemula <xkurnx@gmail.com>
2014-03-10 17:17:04 +0700

    Overview  Template with Sub Nav
    1. add sub nav at header. it will be similar to blog template. should
    support mobile view. this sub nav will show its child pages
    2. add external links at sidebar. if no external link configured for
    that page, then hide the widget
    
    How To Use
    1. Create a new page using "Overview with subnav template" template.
    2. Add the external links as custom fields. The custom field name should
    be "External Link" and you can add multiple fields with this name.
    3.
    Create a new menu named "Overview Subnav" and add the sections you wish
    to appear in the subnav section.
    4. View the page.
    5. You can check the behavior of the sidebar if no
    "External Link" are added.

--- wp-content/themes/tcs-responsive/widget.php
+++ wp-content/themes/tcs-responsive/widget.php
@@ -2,6 +2,7 @@
 /* Register the widget */
 function theme_load_widgets() {
 	register_widget ( 'Related_Content' );
+	register_widget ( 'External_Links' );
 }
 
 
@@ -37,6 +38,47 @@ class Related_Content extends WP_Widget {
 		?>
 						<div class="sideFindRelatedContent">
                                 	<h3>Related Content</h3>
+                                   
+                                </div>
+<!-- /.sideFindRelatedContent -->
+
+<?php
+		echo $after_widget;
+	}
+}
+
+class External_Links extends WP_Widget {
+	
+	/* Widget setup */
+	function External_Links() {
+		/* Widget settings. */
+		$widget_ops = array (
+				'classname' => 'External_Links',
+				'description' => __ ( 'External Links', 'inm' ) 
+		);
+		
+		/* Widget control settings. */
+		$control_ops = array (
+				'id_base' => 'external_links' 
+		);
+		
+		/* Create the widget. */
+		$this->WP_Widget ( 'external_links', __ ( 'External_Links', 'inm' ), $widget_ops, $control_ops );
+	}
+	
+	/* Display the widget */
+	function widget($args, $instance) {
+		extract ( $args );
+		
+		/* Before widget (defined by themes). */
+		echo $before_widget;
+		
+		/* Display the widget title if one was input (before and after defined by themes). */
+		if ($title)
+			echo $before_title . $title . $after_title;
+		?>
+						<div class="sideFindRelatedContent">
+                                	<h3>Links</h3>
                                    
                                 </div>
 <!-- /.sideFindRelatedContent -->
