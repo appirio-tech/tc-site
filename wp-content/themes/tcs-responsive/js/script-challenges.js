135648a5c777cb8318dc2ff971bbada577743e6d
Neil Hastings <neil.hastings@gmail.com>
2014-05-01 07:59:16 -0400

    Remove platforms

--- wp-content/themes/tcs-responsive/js/script-challenges.js
+++ wp-content/themes/tcs-responsive/js/script-challenges.js
@@ -1263,12 +1263,6 @@ appChallenges = {
                     $ul.append('<li><span>' + sp + '</span></li>');
                   });
 
-                  if (!app.isEmptyArray(rec.platforms)) {
-                    $.each(rec.platforms, function(_, sp){
-                      $ul.append('<li><span>' + sp + '</span></li>');
-                    });
-                  }
-
                   $div.append($ul);
                   $('.colCh', row).append($div);
                 }
