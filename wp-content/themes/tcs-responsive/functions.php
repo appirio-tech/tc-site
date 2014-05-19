a4f523a73baaacf2a2e1814a0d8e968d85d9f622
Neil Hastings <neil.hastings@gmail.com>
2014-05-19 12:41:34 -0400

    use php variable for api url

--- wp-content/themes/tcs-responsive/functions.php
+++ wp-content/themes/tcs-responsive/functions.php
@@ -3,6 +3,9 @@
 // Define once because it's faster
 define('THEME_URL', get_template_directory_uri());
 define('CURRENT_FULL_URL', add_query_arg($wp->query_string, '', home_url($wp->request)));
+if (!defined('TC_API_URL')) {
+  define('TC_API_URL', 'https://api.topcoder.com/v2/');
+}
 
 @ini_set('display_errors', 1);
 
--- wp-content/themes/tcs-responsive/js/app/challenge-details/controllers.js
+++ wp-content/themes/tcs-responsive/js/app/challenge-details/controllers.js
@@ -1,24 +1,36 @@
+// @TODO Split out the different parts of the page into different contorllers
 cdapp.controller('CDCtrl', ['$scope', 'ChallengeService', '$sce', function($scope, ChallengeService, $sce) {
   $scope.trust = function(x) {
     return $sce.trustAsHtml(x);
-  }
+  };
+
   $scope.range = function(from, to) {
     var ans = [];
     for (var i = from; i < to; i++) {
       ans.push[i];
     }
     return ans;
-  }
+  };
+
+  // @TODO Move to filter
   $scope.daysLeft = function(seconds) {
     return Math.floor(seconds / (3600 * 24));
-  }
+  };
+
+  // @TODO Move to filter
   $scope.hoursLeft = function(seconds) {
     return Math.floor(Math.floor(seconds % (3600 * 24)) / 3600);
-  }
+  };
+
+  // @TODO Move to filter
   $scope.minsLeft = function(seconds) {
     return Math.floor(Math.floor(seconds % 3600) / 60);
-  }
+  };
+
+  // @TODO Move to filter
   $scope.max = function(x, y) { return x > y ? x : y; };
+
+  // @TODO Move to filter
   $scope.formatDate = function(date) {
     function pad0(x) {
       return (x+'').length == 1 ? '0' + x : x;
@@ -30,7 +42,7 @@ cdapp.controller('CDCtrl', ['$scope', 'ChallengeService', '$sce', function($scop
     var year = date.getFullYear();
     var time = pad0((date.getUTCHours() + 20) % 24) + ':' + pad0(date.getUTCMinutes());
     return month + ' ' + day + ', ' + year + ' ' + time + ' EDT';
-  }
+  };
 
   challengeId = location.href.match(/s\/(\d+)/)[1];
   $scope.round = Math.round;
@@ -51,6 +63,7 @@ cdapp.controller('CDCtrl', ['$scope', 'ChallengeService', '$sce', function($scop
           }
         });
     }
+
     chglo = challenge;
     $scope.challenge = challenge;
     $scope.activeTab = 'overview';
