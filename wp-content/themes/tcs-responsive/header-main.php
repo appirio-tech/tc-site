e9ca2ac9e25c1404754d16f018a795cd7e40740f
Colin Hunt <colin@arcsec.ca>
2014-05-23 12:00:26 -0700

    bugfix I-107071 IE10 compat mode

--- wp-content/themes/tcs-responsive/header-main.php
+++ wp-content/themes/tcs-responsive/header-main.php
@@ -41,9 +41,8 @@ if (isset($_GET['auth']) && $_GET['auth'] == 'logout') {
   <title><?php wp_title(' - ', TRUE, 'right'); ?></title>
   <meta name="description" content="">
   <meta name="author" content="" >
-
-  <meta name="viewport"
-        content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0" />
   <meta name="apple-mobile-web-app-capable" content="yes" />
 
   <!-- Favicons -->
