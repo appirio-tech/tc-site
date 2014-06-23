0aa2bd531e5d71cbfb74ae2a17a77b5dbb02dd7f
James Marquez <jamesmarquez@gmail.com>
2014-06-23 17:39:56 +0800

    fixed prize format issue

--- wp-content/themes/tcs-responsive/js/app/challenge-details/ng-challenge-detail-software.js
+++ wp-content/themes/tcs-responsive/js/app/challenge-details/ng-challenge-detail-software.js
@@ -562,3 +562,14 @@ app.tabNavinit = function() {
     }
   })
 }
+//This function adds number formatting to JS number prototype
+/**
+ * Number.prototype.format(n, x)
+ * 
+ * @param integer n: length of decimal
+ * @param integer x: length of sections
+ */
+Number.prototype.format = function(n, x) {
+  var re = '\\d(?=(\\d{' + (x || 3) + '})+' + (n > 0 ? '\\.' : '$') + ')';
+  return this.toFixed(Math.max(0, ~~n)).replace(new RegExp(re, 'g'), '$&,');
+};
