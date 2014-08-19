d0a6e0eb2d042123fb1b14b978aa5401eba583b0
Tom Ladendorf <tom@tomladendorf.com>
2014-08-19 17:34:27 -0500

    Minor changes to URL structure

--- wp-content/themes/tcs-responsive/js/register-login.js
+++ wp-content/themes/tcs-responsive/js/register-login.js
@@ -756,7 +756,7 @@ $(function () {
       var tcActionValues = tcAction.split('|');
       if (typeof tcActionValues[2] !== 'undefined' && tcActionValues[0] === 'register' && tcActionValues[2] !== '') {
           //add link to challenge-details page for challenge new user tried to register for before making topcoder account
-          $('.thank-you').append("<h4>Are you still interested in participating in the <br>&quot;" + decodeURIComponent(tcActionValues[2]) + "&quot; challenge? <br>If so, <a href='/challenge-details/" + tcActionValues[1] + "/register/'>go there now!</a></h4>");
+          $('.thank-you').append("<h4>Are you still interested in participating in the <br>&quot;" + decodeURIComponent(tcActionValues[2]) + "&quot; challenge? <br>If so, <a href='/challenge-details/register/" + tcActionValues[1] + "/'>go there now!</a></h4>");
         }
     }
   }
