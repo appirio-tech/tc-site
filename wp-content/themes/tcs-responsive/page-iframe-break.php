323e5b063ffd052fd67ee02723a2465422d51334
Thabo <tha.bo.fletcher@gmail.com>
2014-08-27 15:54:13 -0400

    submitting NDA doesnt change NDA status I-124578

--- wp-content/themes/tcs-responsive/page-iframe-break.php
+++ wp-content/themes/tcs-responsive/page-iframe-break.php
@@ -1,8 +1,10 @@
-<?php 
-get_header();
-get_footer(); 
-?>
 <script>
+function getParameterByName(name, source) {
+	name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+	var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+	results = regex.exec(source || location.search);
+	return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
 var loc = getParameterByName('dest') || '/';
-top.window.location = loc;
+top.window.location = unescape(loc);
 </script>
