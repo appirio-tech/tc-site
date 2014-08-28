f4861b5b67fc5e129ec5717f67c7a23e161bf7d0
Thabo <tha.bo.fletcher@gmail.com>
2014-08-28 11:27:26 -0400

    I-124578 - update UX to reflect what is happening

--- wp-content/themes/tcs-responsive/page-iframe-break.php
+++ wp-content/themes/tcs-responsive/page-iframe-break.php
@@ -18,13 +18,16 @@ window.onload = function() {
             oHead.appendChild(arrStyleSheets[i].cloneNode(true));
     }
 
+    parent.document.getElementsByClassName('termsFrame')[0].style.height = '150px';
+
     document.getElementById('loader').style.display = 'block';
     var loc = getParameterByName('dest') || '/';
-	setTimeout(function() {top.window.location = unescape(loc);}, 15000);
+	setTimeout(function() {top.window.location = unescape(loc);}, 21000);
 }
 </script>
 </head>
 <body>
-<div id='loader' class="loading" style="display: none;">Loading...</div>
+<div id='loader' class='loading' style='display: none;'>Loading...</div>
+<p class='text-center' style='margin: 80px 10% 0 10%;'>It may take up to a minute for your agreement to be authorized. If you choose to go back to the terms acceptance page before the authorization is complete, the status may remain unchanged until the process is finished.</p>
 </body>
 </html>
