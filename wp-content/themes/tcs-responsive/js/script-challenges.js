1fc57c00472eac0970d458706dc7f5e8a79ad0cd
wang zhihao <accept.acm@gmail.com>
2014-09-18 07:31:34 +0800

    change the 14px to 13px, hide hours part when days exceed 100

--- wp-content/themes/tcs-responsive/js/script-challenges.js
+++ wp-content/themes/tcs-responsive/js/script-challenges.js
@@ -2617,7 +2617,7 @@ appChallenges = {
     formatTimeLeft: function(seconds, grid) {
         var sep = (grid) ? '' : ' ';
         if (seconds < 0) {
-          return '<span style="font-size:14px;">0' + sep + '<span style="font-size:10px;">Days</span> 0' + sep + '<span style="font-size:10px;">Hrs</span>';
+          return '<span style="font-size:13px;">0' + sep + '<span style="font-size:10px;">Days</span> 0' + sep + '<span style="font-size:10px;">Hrs</span>';
         }
 
         var numdays = Math.floor(seconds / 86400);
@@ -2629,9 +2629,9 @@ appChallenges = {
             style = "color:red";
         }
         if (isNaN(numhours)) {
-            return "<em style='font-size:14px;'>not available</em>";
+            return "<em style='font-size:13px;'>not available</em>";
         }
-        return "<span style='font-size:14px;" + style + "'>" + (numdays > 0 ? numdays + sep + "<span style='font-size:10px;'>Day" + ((numdays > 1) ? "s" : "") + "</span> " : "") + "" + numhours + sep + "<span style='font-size:10px;'>Hrs</span> " + (numdays == 0 ? numminutes + sep + "<span style='font-size:10px;'>Min</span> " : "") + "</span>";
+        return "<span style='font-size:13px;" + style + "'>" + (numdays > 0 ? numdays + sep + "<span style='font-size:10px;'>Day" + ((numdays > 1) ? "s" : "") + "</span> " : "") + "" + (numdays < 100 ? numhours + sep + "<span style='font-size:10px;'> Hr" + ((numhours > 1) ? "s" : "") + "</span> " : "") + (numdays == 0 ? numminutes + sep + "<span style='font-size:10px;'> Min" + ((numminutes > 1) ? "s" : "") + "</span> " : "") + "</span>";
 
     },
 
