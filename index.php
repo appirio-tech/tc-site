897f942bdc3c8d56f06f4039fadfa542fc3e0def
Thabo <tha.bo.fletcher@gmail.com>
2014-09-16 19:41:46 -0700

    I-130796 - sanitize all php superglobals

--- index.php
+++ index.php
@@ -1,4 +1,25 @@
 <?php
+
+foreach ($_GET as $key => $value) {
+	$_GET[$key] = filter_input(INPUT_GET, $key, FILTER_SANITIZE_STRING);
+}
+
+foreach ($_POST as $key => $value) {
+	$_POST[$key] = filter_input(INPUT_POST, $key, FILTER_SANITIZE_STRING);
+}
+
+foreach ($_COOKIE as $key => $value) {
+	$_COOKIE[$key] = filter_input(INPUT_COOKIE, $key, FILTER_SANITIZE_STRING);
+}
+
+foreach ($_SERVER as $key => $value) {
+	$_SERVER[$key] = filter_input(INPUT_SERVER, $key, FILTER_SANITIZE_STRING);
+}
+
+foreach ($_ENV as $key => $value) {
+	$_ENV[$key] = filter_input(INPUT_ENV, $key, FILTER_SANITIZE_STRING);
+}
+
 /**
  * Front to the WordPress application. This file doesn't do anything, but loads
  * wp-blog-header.php which does and tells WordPress to load the theme.
