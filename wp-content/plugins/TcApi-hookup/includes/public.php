2a23f70c7e99b37c46f3dd885799dcfe843b6369
Tom Ladendorf <tom@tomladendorf.com>
2014-11-07 16:11:43 -0600

    Made membercount shortcode return a static value

--- wp-content/plugins/TcApi-hookup/includes/public.php
+++ wp-content/plugins/TcApi-hookup/includes/public.php
@@ -419,7 +419,7 @@ class TCHOOK_Public extends TCHOOK_Plugin {
   // Test Member Count
   function tcapi_get_memberCount($atts, $key = "") {
 
-    return get_activity_summary("memberCount");
+    return "706,649";
   }
 
   /* member stastics  */
