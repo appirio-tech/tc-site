9ff1f0e7d9ed29ac22d11ca9ef779ee743c238b4
vikasrohit <vikasrohit4@yahoo.com>
2015-03-28 12:25:35 +0530

    SUP-194, Challenge details review button
    Enabled Review button for non peer reviewed challenges too.
    
    For non peer reviewed challenges text would be "View Review" and button would be visible if user is logged in and the current phase is "Review" even if user has not submitted the solution. (reverted from commit a0ea571c132287f724fffefa30795a7fda8a25e1)

--- src/js/app/challenge-details/js/controllers/challenge-details-controller.js
+++ src/js/app/challenge-details/js/controllers/challenge-details-controller.js
@@ -39,8 +39,6 @@
     challengeType = $location.search().type;
 
     var vm = this;
-    vm.reviewUrl = '';
-    vm.reviewBtnText = '';
 
     vm.callComplete = false;
     vm.scope = $scope;
@@ -150,7 +148,7 @@
       ChallengeService
           .getChallenge(challengeId)
           .then(function (challenge) {
-            processChallenge(challenge, handle, vm, ChallengeService, $location);
+            processChallenge(challenge, handle, vm, ChallengeService);
             vm.callComplete = true;
             $timeout(function () {
               window.prerenderReady = true;
@@ -164,7 +162,7 @@
       ChallengeService
           .getChallenge(challengeId)
           .then(function (challenge) {
-            processChallenge(challenge, vm.handle, vm, ChallengeService, $location);
+            processChallenge(challenge, vm.handle, vm, ChallengeService);
           });
     }
 
@@ -225,9 +223,8 @@
    * @param challenge
    * @param vm
    * @param ChallengeService
-   * @param $location
    */
-  function processChallenge(challenge, handle, vm, ChallengeService, $location) {
+  function processChallenge(challenge, handle, vm, ChallengeService) {
 
     // Global variable available from ng-page-challenge-details.php
     challengeName = challenge.challengeName;
@@ -358,18 +355,8 @@
     vm.isPeerReviewed = vm.challenge.reviewType === 'PEER';
 
     // update peer review button flag
-    if (handle && vm.inReview) {
-      if ((vm.isPeerReviewed && submitters.indexOf(handle) != -1) || !vm.isPeerReviewed) {
-        vm.challenge.peerReviewDisabled = false;
-      }
-    }
-
-    if (!vm.isPeerReviewed) {
-      vm.reviewBtnText = 'View Review';
-      vm.reviewUrl = 'https://software.' + $location.host() + '/review/actions/ViewProjectDetails?pid=' + vm.challenge.challengeId;
-    } else {
-      vm.reviewBtnText = 'Review This Challenge';
-      vm.reviewUrl = '/peer-reviews/index.html?challenge=' + vm.challenge.challengeId;
+    if (handle && vm.isPeerReviewed && vm.inReview && submitters.indexOf(handle) != -1) {
+      vm.challenge.peerReviewDisabled = false;
     }
 
     vm.hasCheckpoints = vm.numCheckpointSubmissions > 0;
