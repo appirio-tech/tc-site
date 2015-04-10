abb83ac6ef812f433ce498585f2da811f2020b3b
vikasrohit <vikasrohit4@yahoo.com>
2015-04-10 18:30:54 +0530

    SUP-249, Formatting issues on member dashboard
    Fixed issue with mobile view of profile header.
    Added upload photo link.

--- src/js/app/my-dashboard/js/controllers/welcome-back-controller.js
+++ src/js/app/my-dashboard/js/controllers/welcome-back-controller.js
@@ -18,24 +18,37 @@
    * Inject dependencies
    * @type {string[]}
    */
-  WelcomeBackCtrl.$inject = ['$scope', 'ProfileService', 'ChallengeService', 'PHOTO_LINK_LOCATION'];
+  WelcomeBackCtrl.$inject = ['$scope', '$location', 'AuthService', 'ProfileService', 'ChallengeService', 'PHOTO_LINK_LOCATION', 'COMMUNITY_URL'];
 
   /**
    * Controller implementation
    *
    * @param $scope
+   * @param $location location service
    * @param ProfileService services to access profile information via API
    * @param ChallengeService services to access challenge info via API
    * @param PHOTO_LINK_LOCATION base link for user photo
+   * @param COMMUNITY_URL base link for community site
    * @constructor
    */
-  function WelcomeBackCtrl($scope, ProfileService, ChallengeService, PHOTO_LINK_LOCATION) {
+  function WelcomeBackCtrl($scope, $location, AuthService, ProfileService, ChallengeService, PHOTO_LINK_LOCATION, COMMUNITY_URL) {
     var vm = this;
+    vm.communityBaseUrl = $location.protocol() + ":" + COMMUNITY_URL;
     // default rating collor
     vm.ratingColor = "color: #999999";
+    // flag to determine visibility of upload photo link
+    vm.showUploadPhotoLink = false;
+    // url for the upload photo link
+    vm.uploadPhotoLink = null;
+    // count of member stats/metrices to be shown in profile header
+    vm.statsToShow = 2;
 
     // activate controller
-    activate();
+    if (AuthService.isLoggedIn === true) {
+      activate();
+    } else { // if user is not logged in, return (to avoid extra ajax calls)
+      return false;
+    }
 
     function activate() {
       // fetch user info to get handle
@@ -54,6 +67,7 @@
               }
             });
 
+            vm.showUploadPhotoLink = false;
             // Parse user picture link to build photo url
             if (profile && profile.photoLink) {
               if (profile.photoLink.indexOf('//') != -1){
@@ -63,6 +77,15 @@
               }
             } else {
               vm.photoLink = PHOTO_LINK_LOCATION + '/i/m/nophoto_login.gif';  
+              vm.uploadPhotoLink = $location.protocol + ":" + communityBaseUrl + '/tc?module=MyHome';
+              vm.showUploadPhotoLink = true;
+            }
+
+            // calculates the count of metrices to be shown in profile header
+            if (vm.profile.overallEarning > 0) { // earnings should be shown
+              vm.statsToShow = 3;
+            } else { // earnings should not be shown
+              vm.statsToShow = 2;
             }
 
           });
