5ae37c80e37efb017a74f7e84f3b8872100cb062
Thabo <tha.bo.fletcher@gmail.com>
2015-09-21 14:36:12 -0700

    updates to theme from community team

---
+++ wp/wp-content/themes/tcs-responsive/member-onboarding.php
@@ -0,0 +1,188 @@
+<?php
+/*
+Template Name: Member Onboarding
+*/
+?>
+<?php get_header(); ?>
+
+
+        <div class="content">
+            <div id="main" class="memberOnboardingView">
+                <?php if(have_posts()) : the_post();?>
+                        <?php the_content();?>
+                <?php endif; wp_reset_query();?>
+        	
+            
+     <div id="page">
+ 
+    <ul id="filters">
+        <?php
+            $terms = get_terms("member-onboarding-categories");
+            $count = count($terms);
+                //echo '<li><a href="javascript:void(0)" title="" data-filter=".all" class="active">All</a></li>';
+            if ( $count > 0 ){
+ 
+                foreach ( $terms as $term ) {
+ 
+                    $termname = strtolower($term->name);
+                    $termname = str_replace(' ', '-', $termname);
+                    echo '<li><a href="#box-'.$termname.'" title="" class="mk-smooth" >'.$term->name.'</a></li>';
+                }
+            }
+        ?>
+    </ul>
+ 
+    <div id="portfolio">
+ 	<div class="onboarding-body">
+    <div class="container">
+    
+    <?php
+	/*
+	 * Loop through Categories and Display Posts within
+	 */
+	$post_type = 'member-onboarding';
+	 
+	// Get all the taxonomies for this post type
+	$taxonomies = get_object_taxonomies( array( 'post_type' => $post_type,) );
+	 
+	foreach( $taxonomies as $taxonomy ) :
+	 
+		// Gets every "category" (term) in this taxonomy to get the respective posts
+		$terms = get_terms( $taxonomy );
+	 
+		foreach( $terms as $term ) : ?>
+	 	  <?php
+		   $termname = strtolower($term->name);
+           $termname = str_replace(' ', '-', $termname);
+		  ?> 
+          <?php echo '<div id="box-'. $termname .'" class="box-member-onboarding">' ?> 
+		  <?php echo '<h1>'.  $term->name .'</h1>' ?>
+          <?php echo '<h2>'.  $term->description .'</h2>' ?> 
+	 
+			<?php
+			$args = array(
+					'post_type' => $post_type,
+					'posts_per_page' => -1,  //show all posts
+					'order'    => 'ASC',
+					'tax_query' => array(
+						array(
+							'taxonomy' => $taxonomy,
+							'field' => 'slug',
+							'terms' => $term->slug,
+						)
+					)
+	 
+				);
+			$posts = new WP_Query($args);
+	 
+			if( $posts->have_posts() ): while( $posts->have_posts() ) : $posts->the_post(); ?>
+	  
+	  
+                       
+                       <?php 
+					    
+					   
+					    //echo '<div class="all onboarding-item '. $tax .'">';
+						echo '<a class="onboarding-item '. $termname .'" href="';
+						
+						echo get_the_permalink();
+						echo '">';
+						//echo '<a href="'. the_permalink() .'" title="'. the_title_attribute() .'">';
+						echo '<span class="thumbnail">';
+						?>
+                        
+                        <?php if(has_post_thumbnail()) { ?>
+								<?php the_post_thumbnail(); ?>
+						<?php }
+						/* no post image so show a default img */
+						else { ?>
+							   <img src="<?php bloginfo('template_url'); ?>/assets/img/default-img.png" alt="<?php echo get_the_title(); ?>" title="<?php echo get_the_title(); ?>" width="110" height="110" />
+						<?php } ?>
+                        
+						<?php 
+						echo '</span>';
+						echo '<span class="info">';
+						echo '<span class="title">'; 
+						echo get_the_title();
+						echo '</span>';
+						echo '</span>';
+						echo '</a>';
+					   
+					   ?>
+	  
+					   
+	 
+			<?php endwhile; endif; ?>
+         
+         <?php echo '</div>'; ?>
+	 
+		<?php endforeach;
+	 
+	endforeach; ?>
+    
+    <?php
+	    
+       /*$args = array( 'post_type' => 'member-onboarding', 'posts_per_page' => -1 );
+       $loop = new WP_Query( $args );
+         while ( $loop->have_posts() ) : $loop->the_post(); 
+ 
+       $terms = get_the_terms( $post->ID, 'member-onboarding-categories' );						
+            if ( $terms && ! is_wp_error( $terms ) ) : 
+ 
+                $links = array();
+ 
+                foreach ( $terms as $term ) {
+                    $links[] = $term->name;
+                }
+ 
+                $tax_links = join( " ", str_replace(' ', '-', $links));          
+                $tax = strtolower($tax_links);
+            else :	
+	        $tax = '';					
+            endif; 
+ 
+        //echo '<div class="all onboarding-item '. $tax .'">';
+		echo '<a class="all onboarding-item '. $tax .'" href="';
+		echo the_permalink();
+		echo '">';
+        //echo '<a href="'. the_permalink() .'" title="'. the_title_attribute() .'">';
+        echo '<span class="thumbnail">';
+		echo the_post_thumbnail();
+		echo '</span>';
+        echo '<span class="info">';
+		echo '<span class="title">';
+		echo the_title();
+		echo '</span>';
+		echo '</span>';
+		echo '</a>';
+        
+        //echo '<div>'. the_excerpt() .'</div>';
+        //echo '</div>'; 
+      endwhile;*/ ?>
+   </div>
+   </div>
+   </div><!-- #portfolio -->
+ 
+ <div class="onboarding-footer">
+   		<div class="container">
+   		<h2>What can you do with topcoder today?</h2>
+   		<div class="col-left">
+        	<h3>What will you create?</h3>
+            <h4>Jump into a competition now!</h4>
+            <a href="#" class="btn btnBlue">VIEW CHALLENGES</a>
+            
+        </div>
+        
+        <div class="col-right">
+        	<h3>What will you learn?</h3>
+            <h4>Learn more and practice now!</h4>
+            <a href="#" class="btn btnBlue">LEARN MORE</a>
+        </div>
+        </div>
+   </div>
+ 
+  </div><!-- #page -->
+
+<?php get_footer(); ?>
+
+
---
+++ wp/wp-content/themes/tcs-responsive/single-member-onboarding.php
@@ -0,0 +1,160 @@
+<?php
+/**
+ * Template Name: Single Blog
+ */
+?>
+<?php
+
+//require_once ("love-this.php");
+
+get_header ();
+
+$values = get_post_custom ( $post->ID );
+
+$userkey = get_option ( 'api_user_key' );
+$siteURL = site_url ();
+
+$blogPageTitle = get_option("blog_page_title") == "" ? "Welcome to the topcoder Blog" : get_option("blog_page_title");
+?>
+
+<script type="text/javascript">
+	var siteurl = "<?php bloginfo('siteurl');?>";
+</script>
+<div id="onboarding" class="content">
+	<div id="main">
+
+	<?php
+
+	if (have_posts ()) :
+		the_post ();
+		$postId = $post->ID;
+		$quote = get_post_meta ( $post->ID, "Quote", true );
+		$qAuthor = get_post_meta ( $post->ID, "Quote author", true );
+
+		$image = wp_get_attachment_image_src( get_post_thumbnail_id( $postId ), 'single-post-thumbnail' );
+		if($image!=null) $imageUrl = $image[0];
+		else $imageUrl = get_bloginfo('stylesheet_directory')."/i/story-side-pic.png";
+
+		$dateObj = DateTime::createFromFormat('Y-m-d H:i:s', $post->post_date);
+		$dateStr = $dateObj->format('M j, Y');
+
+		$title = htmlspecialchars($post->post_title);
+		$subject = htmlspecialchars(get_bloginfo('name')).' : '.$title;
+		$body = htmlspecialchars($post->post_content);
+		$email_article = 'mailto:?subject='.rawurlencode($subject).'&body='.get_permalink($postId);
+		//Bugfix I-109975: Correct format of twitter blog post shares
+		$twitterShare = createTwitterPost($title, get_permalink($postId));
+		$fbShare = "http://www.facebook.com/sharer/sharer.php?s=100&p[url]=".get_permalink($postId)."&p[images][0]=".$imageUrl."&p[title]=".get_the_title()."&p[summary]=" . urlencode(wrap_content_strip_html(wpautop($title), 130, true,'\n\r',''));
+		$gplusShare = "https://plus.google.com/share?url=".get_permalink($postId);
+
+		$authorObj = get_user_by("id",$post->post_author);
+		$authorLink = get_bloginfo("wpurl")."/author/".$authorObj->user_nicename;
+
+		$categories = get_the_category();
+		$arrCategoriesId;
+		if($categories!=null){
+			foreach($categories as $key=>$category) {
+				$arrCategoriesId[] = $category->term_id;
+			}
+		}
+	?>
+	<!-- Start Overview Page-->
+
+		<!-- page title -->
+		<div class="pageTitleWrapper">
+			<div class="pageTitle container">
+				<h2 class="blogPageTitle"><?php the_title(); ?></h2>
+                <!-- Blog Desc -->
+                <div class="blogDescBox">                    
+                    <!--<div class="postAuthor">By <a href="<?php echo $authorLink; ?>" class="author blueLink"><?php the_author();?></a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;</div>
+                    <div class="postDate">Posted <?php echo $dateStr;?></div>-->
+                    <!-- Social -->
+                    <div class="single-social-section">
+                       <?php /*?> <div class="mk-love-holder"><?php mk_love_this(); ?></div>
+                        <?php if ( get_post_meta( $post->ID, '_disable_comments', true ) != 'false' ) {
+                                echo '<a href="'.get_permalink().'#comments" class="blog-modern-comment"><i class="mk-moon-bubble-9"></i><span> '.get_comments_number( '0', '1', '%').'</span></a>';
+                        } ?>
+						<?php */?>
+                        <div class="blog-share-container">
+                            <div class="blog-single-share mk-toggle-trigger"><i class="mk-moon-share-2"></i></div>
+                            <ul class="single-share-box mk-box-to-trigger">
+                                <li><a class="facebook-share" data-title="<?php $title ?>" data-url="<?php get_permalink() ?>" href="#"><i class="mk-moon-facebook"></i></a></li>
+                                <li><a class="twitter-share" data-title="<?php $title ?>" data-url="'<?php get_permalink() ?>" href="#"><i class="mk-moon-twitter"></i></a></li>
+                                <li><a class="googleplus-share" data-title="<?php $title ?>" data-url="<?php get_permalink() ?>" href="#"><i class="mk-moon-googleplus"></i></a></li>
+                                <li><a class="pinterest-share" data-image="'.$image_src_array[0].'" data-title="<?php $title ?>" data-url="<?php get_permalink() ?>" href="#"><i class="mk-moon-pinterest"></i></a></li>
+                                <li><a class="linkedin-share" data-title="<?php $title ?>" data-url="<?php get_permalink() ?>" href="#"><i class="mk-moon-linkedin"></i></a></li>
+                            </ul>
+                        </div>
+                        <a class="mk-blog-print" onClick="window.print()" href="#" title="<?php __('Print', 'mk_framework') ?>"><i class="mk-moon-print-3"></i></a>
+                        <div class="clearboth"></div>
+                    </div>
+                </div>
+                <a href=".." class="closePost"></a>
+                <div class="clearboth"></div>
+			</div>
+		</div>
+		<!-- page title end -->
+
+
+
+		<article id="mainContent" class="splitLayout overviewPage"> 
+			<div class="container blogPageMainContent">
+				<div class="rightSplit  grid-3-3">
+					<div class="mainStream grid-2-3">
+						<section class="pageContent singleContent">
+                        
+							<!-- content wrapper -->
+							<div id="single-member-onboarding" class="contentWrapper">
+								<?php the_content();?>
+							</div>
+							<!-- content wrapper end -->
+                            
+                            <!-- Author -->
+                           <?php /*?> <?php  if(get_post_meta( $post->ID, '_disable_about_author', true ) != 'false') : ?>
+                            <div class="mk-about-author-wrapper">
+								<div class="avatar-wrapper"><?php global $user; echo get_avatar( get_the_author_meta('email'), '65',false ,get_the_author_meta('display_name', $user['ID'])); ?></div>
+								<div class="mk-about-author-meta">
+								<a class="about-author-name" href="<?php echo get_author_posts_url(get_the_author_meta( 'ID' )); ?>"><?php the_author_meta('display_name'); ?></a>
+								<div class="about-author-desc"><?php the_author_meta('description'); ?></div>
+								<ul class="about-author-social">
+									<?php
+									if(get_the_author_meta( 'twitter' )) {
+										echo '<li><a class="twitter-icon" title="'.__('Follow me on Twitter','mk_framework').'" href="'.get_the_author_meta( 'twitter' ).'"><i class="mk-moon-twitter"></i></a></li>';
+									}
+									if(get_the_author_meta('email')) {
+										echo '<li><a class="email-icon" title="'.__('Get in touch with me via email','mk_framework').'" href="mailto:'.get_the_author_meta('email').'"><i class="mk-moon-envelop"></i></a></li>';
+									}
+									?>
+								</ul>
+								</div>
+								<div class="clearboth"></div>
+                            </div>
+                            <?php endif;  ?><?php */?>
+
+                            <!-- Related Posts -->
+                            <?php do_action('blog_similar_posts', $post->ID); ?>
+                            
+                            <!-- Comments -->
+                            <?php /*?><?php comments_template( '', true ); ?><?php */?>
+
+						</section>
+
+					<?php endif; wp_reset_query();?>
+
+
+						<!-- /.pageContent -->
+					</div>
+					<!-- /.mainStream -->
+					<aside class="sideStream  grid-1-3">
+
+						<?php get_sidebar("member-onboarding"); ?>
+
+					</aside>
+					<!-- /.sideStream -->
+					<div class="clear"></div>
+				</div>
+				<!-- /.rightSplit -->
+			</div>
+		</article>
+		<!-- /#mainContent -->
+<?php get_footer(); ?>
