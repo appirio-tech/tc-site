c341e46590797fc072427032009aeb1c0f5cf9fd
Vikas Agarwal <vikas.agarwal@topcoder.com>
2016-04-01 13:44:02 +0000

    AS#102481437698833, Add NavBar react component to tc-site --Using S3 URl to access the bucket

--- wp/wp-content/themes/tcs-responsive/lib/scripts.php
+++ wp/wp-content/themes/tcs-responsive/lib/scripts.php
@@ -30,7 +30,7 @@ function tcs_responsive_scripts() {
     // 'tc-nav-components' => '//components.' + TC_DOMAIN + '/TCNavComponents.js'
     'moment' => '/js/',
     'react' => '/js/',
-    'tc-nav-components' => 'http://components.topcoder-dev.com'
+    'tc-nav-components' => '//https://s3.amazonaws.com/components.' + TC_DOMAIN
   );
 
   tsc_register_master($assets);
