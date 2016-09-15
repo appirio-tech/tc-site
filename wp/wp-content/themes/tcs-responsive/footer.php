88f4c9fbc3bff929726d6a881a30be9e998d789a
Tom Ladendorf <tom@tomladendorf.com>
2016-09-15 16:48:00 +0200

    removed twitter marketing code

--- wp/wp-content/themes/tcs-responsive/footer.php
+++ wp/wp-content/themes/tcs-responsive/footer.php
@@ -203,19 +203,6 @@ window.onload = function(){
 </script>
 <!-- END Google Retargeting Marketing Code -->
 
-
-<!-- START Twitter Marketing Code -->
-<script src="//platform.twitter.com/oct.js" type="text/javascript"></script>
-<script type="text/javascript">
-twttr.conversion.trackPid('l4r4k');
-</script>
-<noscript>
-<img height="1" width="1" style="display:none;" alt="" src="https://analytics.twitter.com/i/adsct?txn_id=l4r4k&p_id=Twitter" />
-<img height="1" width="1" style="display:none;" alt="" src="//t.co/i/adsct?txn_id=l4r4k&p_id=Twitter" />
-</noscript>
-<!-- END Twitter Marketing Code -->
-
-
 <!-- Start of topcoder Zendesk Widget script -->
 <script>/*<![CDATA[*/window.zEmbed||function(e,t){var n,o,d,i,s,a=[],r=document.createElement("iframe");window.zEmbed=function(){a.push(arguments)},window.zE=window.zE||window.zEmbed,r.src="javascript:false",r.title="",r.role="presentation",(r.frameElement||r).style.cssText="display: none",d=document.getElementsByTagName("script"),d=d[d.length-1],d.parentNode.insertBefore(r,d),i=r.contentWindow,s=i.document;try{o=s}catch(c){n=document.domain,r.src='javascript:var d=document.open();d.domain="'+n+'";void(0);',o=s}o.open()._l=function(){var o=this.createElement("script");n&&(this.domain=n),o.id="js-iframe-async",o.src=e,this.t=+new Date,this.zendeskHost=t,this.zEQueue=a,this.body.appendChild(o)},o.write('<body onload="document._l();">'),o.close()}("https://assets.zendesk.com/embeddable_framework/main.js","topcoder.zendesk.com");
 /*]]>*/</script>
